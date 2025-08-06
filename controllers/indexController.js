d55b46456a313bc4cc95457f7d863c52e8faddbf
Hugo <hquezada@leomail.tamuc.edu>
2025-08-06 15:19:11 -0500

    feat: link category button links to their items

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,5 +4,9 @@ async function getAllCategories(req, res) {
   const categories = await db.getAllCategories();
   res.render("index", { categories });
 }
+async function getCategorySlug(req, res) {
+  const slug = await db.getSlug();
+  return slug;
+}
 
 module.exports = { getAllCategories };
